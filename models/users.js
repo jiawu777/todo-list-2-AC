1dfd68c95324191f01e8b1cba091df75c9c6a3a7
wujia777 <cwu005104@gmail.com>
2022-10-31 23:12:28 +0800

    feat: add user model

---
+++ models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('../config/mongoose')
+const Schema = mongoose.Schema()
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
