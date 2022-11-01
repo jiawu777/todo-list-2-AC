07256ba9a12dc307da8765e55818742533474a11
wujia777 <cwu005104@gmail.com>
2022-11-01 23:27:28 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -15,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use(routes)
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
