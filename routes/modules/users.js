ae5eab6f5212aa407b6523847f58ea970e9c0fb3
wujia777 <cwu005104@gmail.com>
2022-11-03 21:42:30 +0800

    feat: add hash feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 const User = require('../../models/user')
 router.get('/login', (req, res) => {
@@ -41,8 +42,10 @@ router.post('/register', (req, res) => {
                 errors.push({ message: 'Email已被註冊!' })
                 res.render('register', { errors, name, email, password, confirmPassword })
             } else {
-                return User
-                    .create({ name, email, password })
+                return bcrypt
+                    .genSalt(10)
+                    .then(salt => bcrypt.hash(password, salt))
+                    .then(hash => User.create({ name, email, password: hash }))
                     .then(() => res.redirect('/'))
                     .catch(err => console.log(err))
             }
