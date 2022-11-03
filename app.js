ab1517f0b626834df8e9cc14f073f44516fb37ae
wujia777 <cwu005104@gmail.com>
2022-11-03 21:34:47 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')//寫在session之後
-
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -23,11 +23,14 @@ app.use(session({
 }))
 
 usePassport(app)//把app當值丟到passport裡面，在session之後、app.use(routes)之前
-app.use((req, res, next) => {
+app.use(flash()) //掛載套件
+app.use((req, res, next) => {//定義可用於所有views的locals
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error = req.flash('error')
   next()
-
 })
 app.use(routes)
 app.listen(PORT, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -9,11 +9,13 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/users/login'
+    failureRedirect: '/users/login',
+    failureMessage: true
 }))
 
 router.get('/logout', (req, res) => {
     req.logout()//passport提供的功能
+    req.flash('success_msg', '你已成功登出!')
     res.redirect('/users/login')
 })
 
@@ -23,11 +25,21 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
     const { name, email, password, confirmPassword } = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmPassword) {
+        errors.push({ message: '所有欄位都是必填!' })//push object to array，array裡面的是message是object
+    }
+    if (password !== confirmPassword) {
+        errors.push({ message: '密碼與確認密碼不一致!' })
+    }
+    if (errors.length) {
+        return res.render('register', { errors, name, email, password, confirmPassword })
+    }
     User.findOne({ email })
         .then(user => {
             if (user) {
-                console.log('User Already Exists.')
-                res.render('register', { name, email, password, confirmPassword })
+                errors.push({ message: 'Email已被註冊!' })
+                res.render('register', { errors, name, email, password, confirmPassword })
             } else {
                 return User
                     .create({ name, email, password })
@@ -36,6 +48,9 @@ router.post('/register', (req, res) => {
             }
         })
         .catch(err => console.log(err))
+
+
+
 })
 
 module.exports = router
